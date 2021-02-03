8772a3de972b4f61b92aeaabc62fc0d754d77941
yanhao <yh3899@closeli.cn>
2021-02-03 14:38:29 +0800

    上传一个例子100

---
+++ Tree/leetcode100.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Administrator on 2021/2/3.
+//
+/*
+ *
+100. 相同的树
+给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+ *
+ *
+ */
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+   };
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        //把第二个else if的条件经过第一个过滤一下，是一个妙招
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        } else if (p->val != q->val) {
+            return false;
+        } else {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+    }
+};
+//小结
+//把第二个else if的条件经过第一个过滤一下，是一个妙招
